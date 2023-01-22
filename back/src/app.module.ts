64529331a6aee829e75786391c75ccc268fd42c6
tocteman <juanfrancisco.vinueza@gmail.com>
2023-01-21 19:47:14 -0500

    scaffold todos & users

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TodosModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
